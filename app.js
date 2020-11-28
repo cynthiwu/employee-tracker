b7a7f85d3bc83a23139a07d7b5e9ac177d48d0ed
Cynthia Wu <cynthia21wu@gmail.com>
2020-11-28 12:23:10 -0800

    Adding functionality to add employee

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function start() {
 
         switch(response.action) {
             case "View All Employees":
-                allEmployees("ORDER BY ID"); 
+                allEmployees(); 
                 break;
             case "View All Employees by Department":
                 employeeByDept();
@@ -63,9 +63,10 @@ function start() {
 
 const query = "SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', r.title AS Role, d.name AS Department, r.salary AS Salary, CONCAT(m.first_name,' ',m.last_name) AS Manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON r.department_id = d.id LEFT JOIN employee m ON e.manager_id = m.id"
 
+
 function allEmployees(request) {
 
-    connection.query(`${query} ${request}`, function(err, res)
+    connection.query(query, [request], function(err, res)
         {
             if (err) throw err;
             console.table(res);
@@ -73,7 +74,19 @@ function allEmployees(request) {
         })      
 }
 
+
 function employeeByDept() {
+
+    const departments = connection.query("SELECT * FROM department");
+    console.log(departments);
+
+    inquirer.prompt({
+        name:"department",
+        type: "list",
+        message: "Please select a Department.",
+        choices: departments
+    });
+
     deptChoice().then(([rows]) => {
         console.log(rows);
         const departments = rows.map(r => r.name)
@@ -92,7 +105,7 @@ function employeeByDept() {
 
 function employeeByManage() {
 
-    managerChoice().then(([rows]) => {
+    employeeChoice().then(([rows]) => {
         console.log(rows);
         let employees = rows.map(r => ({name: r.first_name + " " + r.last_name, value: r.id}));
         let managerIds = rows.map(r => r.manager_id);
@@ -112,12 +125,13 @@ function employeeByManage() {
     })
 }
 
-// Need to figure out how to add a second option of choices 
 function addEmployee() {
-    roleChoice().then(([rows]) => {
+    employeeChoice().then(([rows]) => {
         console.log(rows);
-        let roles = rows.map(r => r.title);
-        console.log(roles);
+        let roleId = rows.map(r => r.role_id);
+
+        let employees = rows.map(r => r.id);
+        employees.push("null");
         inquirer.prompt([
             {
             name:"first_name",
@@ -130,14 +144,19 @@ function addEmployee() {
             message:"What is the employee's last name?"
             },
             {
-            name:"role",
+            name:"role_id",
             type:"list",
-            message:"What is the employee's role?",
-            choices: roles
+            message:"Please select a role ID",
+            choices: roleId
             },
-
-            ]).then(function(response) {
-                console.log(response)
+            {
+            name:"manager",
+            type:"list",
+            message:"Please select a manager ID?",
+            choices: employees
+            }
+        ]).then(function(response) {
+            createEmployee(response.first_name, response.last_name, response.role_id, response.manager)
         })
     })
 }
@@ -147,8 +166,18 @@ function addEmployee() {
 function updateRole() {
     employeeChoice().then(([rows]) => {
         console.log(rows);
-        let employees = rows.map(r => r.name);
-        console.log(employees);
+        let employees = rows.map(r => ({name: r.first_name + " " + r.last_name, value: r.id}));
+        let employeeName = employees.map(r => r.name);
+        inquirer.prompt([
+            {
+            name:"employee",
+            type:"list",
+            message:"Which employee which you like to update?",
+            choices: employeeName
+            }
+        ]).then(function(response) {
+            console.log(response)
+        })
     })
 }
 
@@ -165,7 +194,7 @@ function deptChoice() {
     return connection.promise().query("SELECT name FROM department")
 }
 
-function managerChoice() {
+function employeeChoice() {
     return connection.promise().query("SELECT * FROM employee")
 }
 
@@ -173,8 +202,17 @@ function roleChoice() {
     return connection.promise().query("SELECT title FROM role")
 }
 
-function employeeChoice() {
-    return connection.promise().query("SELECT CONCAT(first_name,' ',last_name) AS name FROM employee")
+function createEmployee(first_name, last_name, role_id, manager) {
+    if (manager === "null") {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)", [`${first_name}`, `${last_name}`, `${role_id}`], function(err,res) {
+            if (err) throw err;
+            start();
+            }
+        )
+    } else {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [`${first_name}`, `${last_name}`, `${role_id}`, `${manager}`], function(err,res) {
+            if (err) throw err;
+            start();
+        })
+    }    
 }
-
-
